916d82c7aa10d9222de5dfa4b6c7befc038ef3ad
Ajay <ajayh9871@gmail.com>
2026-07-09 15:12:36 +0530

    added f3.java file to master

---
+++ f3.java
@@ -0,0 +1,13 @@
+public class f3 {
+	public static void main(String[] args) {
+		System.out.println("Hello from f3");
+		// example usage
+		int a = 3, b = 5;
+		System.out.println(a + " + " + b + " = " + add(a, b));
+	}
+
+	public static int add(int x, int y) {
+		return x + y;
+	}
+}
+    
